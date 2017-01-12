1a0d9ab260879bfb2cf87cc2fd9424f379a9796c
muyuu <anticyborg@gmail.com>
2017-01-13 00:21:29 +0900

    use stateless functional component

--- src/js/components/auth/Logout.js
+++ src/js/components/auth/Logout.js
@@ -2,33 +2,25 @@ import React, {Component} from 'react';
 
 import {action, store} from '../../dispatcher/dispatcher';
 
-export default class Logout extends Component {
-    constructor(props){
-        super(props);
+export default props =>{
 
-        // binding
-        this.logout = this.logout.bind(this);
-    }
-
-    logout(){
+    const logout = ()=>{
         action.logout();
-    }
+    };
 
-    render(){
-        return (
-            <div className="logoutBox">
-                <div className="logoutBox__head">
-                    <div className="logoutBox__caption">Log out</div>
-                </div>
-                <div className="logoutBox__btn">
-                    <button
-                        id="logout"
-                        onClick={this.logout}
-                    >
-                        log out
-                    </button>
-                </div>
+    return (
+        <div className="logoutBox">
+            <div className="logoutBox__head">
+                <div className="logoutBox__caption">Log out</div>
             </div>
-        );
-    }
-}
+            <div className="logoutBox__btn">
+                <button
+                    id="logout"
+                    onClick={logout}
+                >
+                    log out
+                </button>
+            </div>
+        </div>
+    );
+};
