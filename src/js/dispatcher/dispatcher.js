223df38fde60aad1ccea9d9b7f7ce14ce5af0440
muyuu <anticyborg@gmail.com>
2017-01-14 15:01:14 +0900

    combine divided store

--- src/js/dispatcher/dispatcher.js
+++ src/js/dispatcher/dispatcher.js
@@ -5,5 +5,13 @@ const dispatcher = new EventEmitter();
 import ActionCreator from '../actions/Actions';
 import Sotre from '../stores/Stores';
 
+// set stores
+import authSotre from '../stores/auth/authStores';
+const stores = Object.assign(
+    {},
+    authSotre
+);
+
 export const action = new ActionCreator(dispatcher);
-export const store = new Sotre(dispatcher);
+export const store = new Sotre(dispatcher, stores);
+
