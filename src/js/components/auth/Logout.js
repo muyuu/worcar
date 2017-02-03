a227c982c0abb3c6cc54064295885817d42fc889
muyuu <anticyborg@gmail.com>
2017-02-03 10:20:05 +0900

    add overlay div

--- src/js/components/auth/Logout.js
+++ src/js/components/auth/Logout.js
@@ -9,18 +9,20 @@ export default props =>{
     };
 
     return (
-        <div className="inputsBox">
-            <div className="inputsBox__head">
-                <div className="inputsBox__caption">Log out?</div>
-            </div>
-            <div className="inputsBox__btn">
-                <button
-                    id="logout"
-                    className="btn btn-block"
-                    onClick={logout}
-                >
-                    log out
-                </button>
+        <div className="overlay">
+            <div className="inputsBox">
+                <div className="inputsBox__head">
+                    <div className="inputsBox__caption">Log out?</div>
+                </div>
+                <div className="inputsBox__btn">
+                    <button
+                        id="logout"
+                        className="btn btn-block"
+                        onClick={logout}
+                    >
+                        log out
+                    </button>
+                </div>
             </div>
         </div>
     );
